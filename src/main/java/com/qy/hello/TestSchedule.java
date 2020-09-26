6aefe0b908c0a77f5958e596e973ddd277b1e844
Li Aixiong <liaixiong@sina.com>
2020-09-26 16:08:39 +0800

    add debug info

--- src/main/java/com/qy/hello/TestSchedule.java
+++ src/main/java/com/qy/hello/TestSchedule.java
@@ -12,10 +12,10 @@ public class TestSchedule {
 
   @Scheduled(fixedDelay = 10*1000)
   public void test(){
-//    System.out.println("定时任务执行开始!");
-//    System.out.println("定时任务执行结束!");
-    log.info("====>定时任务开始");
-    log.info("====>定时任务结束");
+    System.out.println("定时任务执行开始!");
+    System.out.println("定时任务执行结束!");
+    log.info("====>hello:定时任务开始");
+    log.info("====>hello:定时任务结束");
   }
 }
 
